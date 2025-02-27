356959453c41f3c9a54c7925bcdd73d13c5bc493
noomsloof <y.foolsmoon@gmail.com>
2025-02-27 21:16:43 +0700

    1st

---
+++ bookcollection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bookcollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
